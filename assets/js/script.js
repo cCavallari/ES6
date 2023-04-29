54cf12b4fcf2d9c745c7b1f8a1afa347c1570c30
Catalina Cavallari <cavallari.-@hotmail.com>
2023-04-29 12:59:42 -0400

    trim

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,28 @@ btnEnviar.addEventListener('click', function(){
     cajaUno.value = cajaUno.value.replaceAll('esto', `esta cadena de caracteres ${nombre}`);
     console.log(cajaUno.value);
 
+    /* expresiones regulares */
+    /* En la caja dos, toda letra "a" que se incorpore se deben cambiar de mayúsculas a minúsculas y vicerversa */
+    /* Object(); función contructora MADRE */
+    /* gi = es una bandera que evalua la cadena de caracteres (global) sin importar si son mayúsculas o minúsculas (i)  */
+    /* Tenemos dos opciones: */
+    var regexUno = new RegExp(/a/, 'gi');
+    var regexDos = /hola/gi;
+
+    cajaDos.value = cajaDos.value.replaceAll(regexUno, function(match){ /* match es una convención, ya que este parametro puede llevar cualquier nombre */
+        return match === 'a' ? 'A': 'a';
+    });
+
+    if (match === 'a') {
+        return 'A';
+    } else if(match === 'c'){
+        return 'C';
+    }
+
     /* alert('Etse alert se envia después de la captura de cajas'); */
 });
+
+/* trimStart . trimEnd (limpia los espacios en balnco del inicio y el final, respectivamente, de la cadena de caracteres)*/
+cajaUno.value = cajaUno.value.trimEnd();
 
 /* Template string - back ticks*/
